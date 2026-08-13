3266aae23048a92a110144e42c803a88d2cfd0e2
Priyanka Pandit <priyankapandit0012@gmail.com>
2026-08-13 18:48:01 +0530

    add customer

---
+++ customer.rb
@@ -0,0 +1,17 @@
+class customer
+    def display_menu
+        
+            puts "E-commerce website"
+            puts " Press 1  To View Product"
+            puts " Press 2  To Search Product details"
+            puts " Press 3  To Add Products to cart"
+            puts " Press 4  To Remove Product from cart"
+            puts " Press 5  To Update Product quantity"
+            puts " Press 6  To View cart"
+            puts " Press 7  To Place Orders"
+            puts " Press 8  To View Order history"
+            puts " Press 9 To exit"
+            puts "choose between 1-9"
+        
+    end
+end
